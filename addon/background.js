ac2a2a78e8084663d1cc55e415a779290d4aea8a
Shubham Singh <shubham.singh@mygstcafe.in>
2025-01-18 18:45:21 +0530

    Add: Support for connection-less messaging between native app and content script

--- addon/background.js
+++ addon/background.js
@@ -2,13 +2,14 @@ console.log("background2.js");
 /*
  background script acts as a bridge between content script and native application
 */
+const nativeAppName = "baremetal";
 let nativeAppPort;
 const dataChunks = [];
 browser.runtime.onConnect.addListener((contentScriptPort) => {
   contentScriptPort.onMessage.addListener(async (message) => {
     // pass the message to native application
     if (!nativeAppPort) {
-      nativeAppPort = browser.runtime.connectNative("baremetal");
+      nativeAppPort = browser.runtime.connectNative(nativeAppName);
 
       // listen for messages from native application
       nativeAppPort.onMessage.addListener(async (message) => {
@@ -54,5 +55,31 @@ browser.runtime.onConnect.addListener((contentScriptPort) => {
       "\nSending to native app"
     );
     nativeAppPort.postMessage(message);
+  });
+});
+
+browser.runtime.onMessage.addListener((message, sender) => {
+  const dataChunks = [];
+  browser.runtime.sendNativeMessage(nativeAppName, message).then((response) => {
+    switch (response.status) {
+      case 0:
+        browser.tabs.sendMessage(sender.tab.id, {
+          status: 0,
+          data: response.data,
+          id: message.id,
+        });
+        dataChunks.length = 0;
+        break;
+      case 0.5:
+        dataChunks.push(response.data);
+        break;
+      case 1:
+        browser.tabs.sendMessage(sender.tab.id, {
+          status: 1,
+          data: response.data,
+          id: message.id,
+        });
+        break;
+    }
   });
 });
--- addon/script.js
+++ addon/script.js
@@ -27,9 +27,39 @@ function NativeFunctions() {
           });
         }
     );
+  else {
+    const genId = () => crypto.getRandomValues(new Uint8Array(4)).join("");
+    return Object.values(arguments).map((functionName) => {
+      return function () {
+        return new Promise((resolve, reject) => {
+          const id = genId();
+          const message = {
+            functionName,
+            arguments: Object.values(arguments),
+            id,
+          };
+          const listener = (message) => {
+            if (message.id === id) {
+              if (message.status === 0) {
+                resolve(message.data);
+              } else {
+                reject(message.data);
+              }
+              browser.runtime.onMessage.removeListener(listener);
+            }
+          };
+          browser.runtime.onMessage.addListener(listener);
+          browser.runtime.sendMessage(message);
+        });
+      };
+    });
+  }
 }
 
-const [getTheme, getWallpaper] = NativeFunctions("getTheme", "getWallpaper");
+const [getTheme, getWallpaper] = new NativeFunctions(
+  "getTheme",
+  "getWallpaper"
+);
 
 getTheme("theme")
   .then((theme) => {
